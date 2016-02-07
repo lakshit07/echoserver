9f4c1adea87438668a26f0fc9a3da432b6abed43
Lakshit Bhutani <lakshitbhutani11@gmail.com>
2016-02-08 00:16:28 +0530

    initial commit

---
+++ app/controllers/request_controller.rb
@@ -0,0 +1,16 @@
+class RequestController < ApplicationController
+
+	def info
+		# @contact = Contact.new
+		@server = request.env["SERVER_NAME"]
+		@user_agent = request.env["HTTP_USER_AGENT"]
+		@path = request.env["PATH_INFO"]
+		@encoding = request.env["HTTP_ACCEPT_ENCODING"]
+		@protocol = request.env["SERVER_PROTOCOL"]
+		@cookie = request.env["HTTP_COOKIE"]
+		# @query = request.env["QUERY_STRING"]
+		@reqm = request.env["REQUEST_METHOD"]
+		@conn = request.env["HTTP_CONNECTION"]
+	end	
+
+end
